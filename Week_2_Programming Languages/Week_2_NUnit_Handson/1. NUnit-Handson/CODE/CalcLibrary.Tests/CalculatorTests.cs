5e02eca47bad28fa3b1b32e452e77a9b203765f0
Sayantan211 <sayantan97351@gmail.com>
2025-06-29 16:36:57 +0530

    Week-2

---
+++ Week_2_Programming Languages/Week_2_NUnit_Handson/1. NUnit-Handson/CODE/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using CalcLibrary;
+using NUnit.Framework;
+
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void SetUp()
+        {
+            calc = new Calculator();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up if needed
+        }
+
+        [TestCase(2, 3, 5)]
+        [TestCase(0, 0, 0)]
+        [TestCase(-1, -1, -2)]
+        public void Add_ShouldReturnCorrectSum(int a, int b, int expected)
+        {
+            int result = calc.Add(a, b);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ Week_2_Programming Languages/Week_2_NUnit_Handson/1. NUnit-Handson/CODE/CalcLibrary.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcLibrary.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcLibrary.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e7bc3b5a-1d70-4ace-be22-fb0b8a74021c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
